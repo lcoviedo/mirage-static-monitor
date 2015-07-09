d85934d4fd4eab013eb41c6409fa295cd85dfbdf
lcoviedo <psxlco@nottingham.ac.uk>
2015-07-09 23:35:27 +0100

    scale down based on served objects, get mac as vm name, and control policies implemented

--- dispatch.ml
+++ dispatch.ml
@@ -13,46 +13,51 @@ let t0 = ref 0.0
 let t1 = ref 0.0
 let i = ref 0
 let d_array = Array.make 64 0.0
-let counter = ref 0
-let cid1 = ref 0
+let replicate_flag = ref true                                                    (* Flag to replicate itself only once *)
+let timeout_flag = ref false                                                     (* only possible to request after timeout expires *)
+let burst_count = ref 0
+let obj_count = ref 0
+let oid1 = ref 0
+let vm_name = ref ""
 
-let h_load = 0.00007                                                                  (* High threshold value *)
-let irmin_ip = "10.0.0.1"                                                             (* Location of irmin store *)
+let h_load = 0.00019                                                             (* High threshold value *)
+let irmin_ip = "128.232.80.10"                                                   (* Location of irmin store *)
 
-let uri = Uri.of_string "http://irmin:8080/update/jitsu/vm/request/"                  (* Path for vm requests*)
-let add_vm = `String "{\"params\":\"add_vm\"}"                                        (* Parameters to be defined *)
-let destroy_vm = `String "{\"params\":\"destroy_vm\"}"
+(* Request parameters *)
+let add_vm = `String "{\"params\":\"add\"}"
+let delete_vm = `String "{\"params\":\"delete\"}"                                     
 
 
-module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) = struct
+module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
 
   module H   = Cohttp_mirage.Server(Conduit_mirage.Flow)
   module HTTP = Cohttp_mirage.Client
    
   let conduit = Conduit_mirage.empty
   let stackv4 = Conduit_mirage.stackv4 (module S)
-   
+  
   (* Manual resolve for irmin*) 
   let irmin_store =
     let hosts = Hashtbl.create 3 in
     Hashtbl.add hosts "irmin"
       (fun ~port -> `TCP (Ipaddr.of_string_exn irmin_ip, port));
     hosts  
-    
-  (* Store a parameter on Irmin *)     
-  let http_post c ctx req=
-    C.log_s c (sprintf "Posting a value %s in Irmin" (Uri.to_string uri)) >>= fun () ->
+  
+  (* Send request to Irmin *)     
+  let http_post c ctx req =    
+    let uri = (Uri.of_string ("http://irmin:8080/update/jitsu/request/params/" ^ !vm_name ^ "/action")) in
+    C.log_s c (sprintf "Posting in path %s" (Uri.to_string uri)) >>= fun () ->
     HTTP.post ~ctx ~body:req uri >>= fun (resp, body) ->
     Cohttp_lwt_body.to_string req >>= fun body ->
-    C.log_s c (sprintf "%s" body)
-   
+    C.log_s c (sprintf "%s" body) 
+    
   (* Conduit connection helper *)
-  let conduit_conn c stack req=
+  let conduit_conn c stack req =
     Lwt.ignore_result (
       lwt conduit = Conduit_mirage.with_tcp Conduit_mirage.empty stackv4 stack in
         let res = Resolver_mirage.static irmin_store in
         let ctx = HTTP.ctx res conduit in
-        http_post c ctx req) 
+        http_post c ctx req)
    
   (* Monitor load based on request/reply times *)
   let scale_up c stack =  
@@ -60,34 +65,38 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) = struct
       t1 := Time.Monotonic.to_seconds(Time.Monotonic.time());       
       let delay = !t1 -. !t0 in
       d_array.(!i) <- delay;
-      (*C.log c (Printf.sprintf "t0 = %f  t1= %f  delay = %f" !t0 !t1 delay);*)       (* For debugging *)                
+      (*C.log c (sprintf "delay = %f" delay);*)       (* For debugging *)                
       if !i < (Array.length d_array - 1) 
         then incr i 
         else (
           i := 0;      
           let avg = 
             (Array.fold_right (+.) d_array 0.0) /. float(Array.length d_array) in
-          if avg > h_load then (                                                      (* above that ~ %80 cpu and likely to crash*)
-            (*C.log c (Printf.sprintf "HIGH LOAD.......%f" avg);*)                    (* For debugging *)
-            conduit_conn c stack add_vm)
+          if (!replicate_flag = true && !timeout_flag = true) then ( 
+            if avg > h_load then (                                                      (* above that ~ %80 cpu and likely to crash *) 
+              C.log c (sprintf "CREATE REPLICA....Delay=%f  counter=%d" avg !burst_count);         (* For debugging *)
+              incr burst_count;                                                       (* avoid replica request on a single burst *)
+              if !burst_count > 1 then ( 
+                replicate_flag := false;                                              
+                conduit_conn c stack add_vm) ) ) )
         )
-     )
         
   (* Monitor idleness of unikernel *)        
-  let rec scale_down c stack n =                                                      (* if idle for 5 secs then trigger action *)
-      cid1 := !counter;                                                               (* !counter holds the current value of cids *)
-      Time.sleep n >>= fun () ->   
-        if !cid1 = !counter then (
-          (*C.log c (Printf.sprintf "LOW LOAD -> destroy replica");*)                 (* For debugging *)           
-          conduit_conn c stack destroy_vm
-          )
-          else C.log c (
-                 Printf.sprintf "New conns in last 5s: %d" (!counter - !cid1));       (* For debugging *)
-      scale_down c stack n 
+  let rec scale_down c stack n =                                                      
+    oid1 := !obj_count;                                                 (* if idle for 3 secs then trigger action *)
+    Time.sleep n >>= fun () ->   
+      if !oid1 = !obj_count then (
+        conduit_conn c stack delete_vm;                                  (* add flag for deleting once? *)
+        C.log c (sprintf "LOW LOAD -> delete replica")                 (* For debugging *)           
+        )
+        else C.log c (
+               sprintf "Objs requested in last 3s: %d" (!obj_count - !oid1));       (* For debugging *)
+    scale_down c stack n
 
-
-  let start c fs stack = 
+  (* START *)
+  let start c fs stack n0 = 
    Lwt.join[(
+    let _ = vm_name := Macaddr.to_string (N0.mac n0) in  
     let read_fs name =
       FS.size fs name >>= function
       | `Error (FS.Unknown_key _) -> fail (Failure ("read " ^ name))
@@ -114,31 +123,31 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) = struct
       | segments -> 
         let path = String.concat "/" segments in 
         Lwt.catch (fun () -> 
-          read_fs path >>= fun body ->                                                                       
-          H.respond_string ~status:`OK ~body () 
+          read_fs path >>= fun body ->
+          incr obj_count;                                                                       
+          H.respond_string ~status:`OK ~body ()           
           )  (fun exn -> 
           H.respond_not_found ()
           )
-    in 
-          
+    in
     (* HTTP callback *)
     let callback conn_id request body =    
       t0 := Time.Monotonic.to_seconds(Time.Monotonic.time());
       let uri = Cohttp.Request.uri request in
       let reply = dispatcher (split_path uri) in
-      let _ = scale_up c stack in
+      let _ = scale_up c stack in                                        (* response time per object *)
       reply 
     in 
-    
     let conn_closed (_,conn_id) =
-      let cid = Cohttp.Connection.to_string conn_id  in
-      C.log c (Printf.sprintf "conn %s closed" cid);
-      counter := int_of_string(cid); 
-      in
-        
+      (*let cid = Cohttp.Connection.to_string conn_id  in
+      C.log c (sprintf "conn %s closed" cid);                    (* For debugging *)
+      *)
+      () in   
       Conduit_mirage.with_tcp conduit stackv4 stack >>= fun conduit ->
       let spec = H.make ~conn_closed ~callback:callback () in
-      Conduit_mirage.listen conduit (`TCP 80) (H.listen spec)); 
-      (scale_down c stack 5.0)]                                                       (* Scale down monitor thread *)
+      Conduit_mirage.listen conduit (`TCP 80) (H.listen spec));                     (**TODO: modular threads *)
+      lwt _ = Time.sleep 30.0 >> Lwt.return(timeout_flag := true) in ();            (* 30s delay for replica request *) 
+      lwt _ = Time.sleep 5.0 >> Lwt.return(burst_count := 0) in ();                 (* reset counter for incoming burst traffic *) 
+      (scale_down c stack 3.0);]                                                    (* Scale down monitor thread *)
           
 end
