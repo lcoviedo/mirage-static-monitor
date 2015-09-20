41b75972c3546b43b17f0e3aecd5c24b535603c0
lcoviedo <psxlco@nottingham.ac.uk>
2015-09-20 17:30:42 +0100

    log entries added

--- dispatch.ml
+++ dispatch.ml
@@ -12,6 +12,7 @@ let blue fmt   = sprintf ("\027[36m"^^fmt^^"\027[m")
 let t0 = ref 0.0
 let t1 = ref 0.0
 let t_avg = ref 0.0
+let log = ref ""
 let i = ref 0
 let j = ref 0
 let delay_array = Array.make 64 0.0
@@ -20,13 +21,14 @@ let oid = ref 0
 let irmin_task = "{\"task\":{\"date\":\"0\",\"uid\":\"0\",\"owner\":\"\",\"messages\":[\"write\"]},\"params\":\""
 let avg_flag = ref false  (*flag to sum up consecutive avg values once the 16 array is full*)
 let sum_avg_array = Array.make 16 0.0
-let avg_array = Array.make 256 (0.0, 0.0)  (* For expermiental purposes *)
+let avg_array = Array.make 128 (0.0, 0.0)  (* For expermiental purposes *)
 let avg_counter = ref 0
 let exp_no = ref 0
+let replicate_flag = ref true (** single unikernel experiment *)
 let high_load = 0.000100  (* 100 micro seconds - High threshold value *)
 let low_load = 150  (* average object request per second rate  - Low threshold value *)
 let max_objreq = 385.0
-let irmin_ip = "128.232.80.10" (*"10.0.0.1"*)  (* Location of irmin store *)
+let irmin_ip = (*"128.232.80.10"*) "10.0.0.1"  (* Location of irmin store *)
 let irmin_port = ref 0
 let irmin_1 = ["12:43:3d:a3:d3:02"; "12:43:3d:a3:d3:03"; "12:43:3d:a3:d3:04"; "12:43:3d:a3:d3:05"; 
                "12:43:3d:a3:d3:06"; "12:43:3d:a3:d3:07"; "12:43:3d:a3:d3:08"; "12:43:3d:a3:d3:09";
@@ -58,7 +60,7 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
   let http_get c stack uri =
     lwt (conduit, ctx) = conduit_conn stack in
     (*C.log_s c (sprintf "Fetching %s:" (Uri.to_string uri)) >>= fun () ->*) (* debugging *)
-    HTTP.get ~ctx uri >>= fun (response, body) ->
+    HTTP.get ~ctx uri >>= fun (resp, body) ->
     Cohttp_lwt_body.to_string body >>= fun body ->
       (*let _ = C.log_s c (sprintf "Response: %s" (body)) in*) (* debugging *)
       let str = Re_str.replace_first (Re_str.regexp ".*\\[\"") "" body in
@@ -73,7 +75,8 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
       (*C.log_s c (sprintf "Posting in path %s" (Uri.to_string uri)) >>= fun () ->*) (* debugging *)
       HTTP.post ~ctx ~body:req uri >>= fun (resp, body) ->
       Cohttp_lwt_body.to_string req >>= fun body -> 
-      C.log_s c (sprintf ("Posting:%s in path %s") body (Uri.to_string uri)) (* debugging *)
+      C.log_s c (sprintf ("Posting in path %s") (Uri.to_string uri) ) (* debugging *)
+      (*C.log_s c (sprintf ("Body:%s Path:%s") body (Uri.to_string uri))*)
       )
   
   (* Monitor-Scale up based on request/reply times *)
@@ -101,17 +104,18 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
               (Array.fold_right (+.) sum_avg_array 0.0) /. float(Array.length sum_avg_array) in (* Sum Avg of 16 values of 64 avgs *)
               t_avg := Clock.time();  (* part of experiment *)
               (*C.log c (sprintf "Avg delay = %f" sum_avg);*) (* debugging *)
-              if sum_avg >= high_load then (
+              if sum_avg >= high_load && !replicate_flag then (
               let rpc_add = Rpc.Enum [
                   Rpc.rpc_of_string "add_vm";
-                  Rpc.rpc_of_string vm_name;
+                  (*Rpc.rpc_of_string vm_name;*)
                   Rpc.rpc_of_string "static-web";
                 ] in
               let add = Rpc.to_string rpc_add in
               let add_vm = `String (irmin_task ^ add ^ "\"}") in
               let uri = (Uri.of_string ("http://irmin/update/jitsu/request/" ^ vm_name ^ "/request")) in
-              http_post c stack uri add_vm;
+              (*http_post c stack uri add_vm;*)
               C.log c (sprintf "CREATE REPLICA........");
+              replicate_flag := false; (* single unikernel experiment *)
             );
           (* Experiment - Post latency values *)
           let avg_tuple = (!t_avg,sum_avg) in
@@ -129,7 +133,7 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
             let rpc_avg_string = Rpc.to_string rpc_avg_array in
             let avg_rpc = `String (irmin_task ^ rpc_avg_string ^ "\"}") in
             let uri = (Uri.of_string ("http://irmin/update/jitsu/exp/" ^ vm_name ^ "/data" ^ (string_of_int !exp_no))) in
-            (*conduit_conn c stack avg_rpc uri; )*)
+            http_post c stack uri avg_rpc;
             http_post c stack uri avg_rpc; )
            )
          )
@@ -151,7 +155,7 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
     if avg_objreq < low_load then (
       let rpc_del = Rpc.Enum [
           Rpc.rpc_of_string "del_vm";
-          Rpc.rpc_of_string vm_name;
+          (*Rpc.rpc_of_string vm_name;*)
          ] in
       let del = Rpc.to_string rpc_del in
       let del_vm = `String (irmin_task ^ del ^ "\"}") in
@@ -174,6 +178,7 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
     lwt initial_xs = http_get c stack uri in
     Hashtbl.add host_table "initial_xs"     
       (fun ~port -> `TCP (Ipaddr.of_string_exn initial_xs, 8080));
+    
     Lwt.join[(
         let read_fs name =
           FS.size fs name >>= function
@@ -183,7 +188,6 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
             | `Error (FS.Unknown_key _) -> fail (Failure ("read " ^ name))
             | `Ok bufs -> return (Cstruct.copyv bufs)
         in
-
         (* Split a URI into a list of path segments *)
         let split_path uri =
           let path = Uri.path uri in
@@ -194,24 +198,38 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
           List.filter (fun e -> e <> "")
             (aux (Re_str.(split_delim (regexp_string "/") path)))
         in
-
         (* dispatch non-file URLs *)
         let rec dispatcher = function
           | [] | [""] -> dispatcher ["index.html"]
           | segments -> 
             let path = String.concat "/" segments in 
+            incr obj_count;
             Lwt.catch (fun () -> 
-                read_fs path >>= fun body ->
-                incr obj_count;
-                H.respond_string ~status:`OK ~body ()
+                read_fs path >>= fun body ->                
+                log := !log ^ "200 OK\n";
+                C.log c (sprintf "After serving:\n%s" !log);
+                (* Extract body length here *)
+                H.respond_string ~status:`OK ~body ()   
               )  (fun exn ->
+                log := !log ^ "Not Found\n";
+                C.log c (sprintf "Error entry:\n%s" !log);
                 H.respond_not_found ()
               )
         in
         (* HTTP callback *)
         let callback conn_id request body =
           t0 := Time.Monotonic.to_seconds(Time.Monotonic.time());
+          (* Unsupported gmtime on Xen, issue #333
+          let date = Clock.gmtime(Clock.time ()) in  (* day/month/year:hour:min:sec *)
+          let (day, month, year, hour, min, sec) = (date.tm_mday, date.tm_mon, date.tm_year, date.tm_hour, date.tm_min, date.tm_sec) in  
+          (sprintf "%d/%d/%d:%d:%d:%d" date.tm_mday date.tm_mon date.tm_year date.tm_hour date.tm_min date.tm_sec)
+          *)
           let uri = Cohttp.Request.uri request in
+          let meth = Cohttp.Code.string_of_method (Cohttp.Request.meth request) in
+          let headers = Cohttp.Header.to_string (Cohttp.Request.headers request) in
+          (*lwt body_size = body |> Cohttp_lwt_body.to_string >|= fun body ->
+          String.length body in*)
+          log := !log ^ (sprintf "%f" !t0) ^ (Uri.to_string uri) ^ meth ^ headers (*^ (string_of_int body_size)*);
           let reply = dispatcher (split_path uri) in
           let _ = scale_up c stack vm_name in (* response time per object *)
           reply
@@ -219,7 +237,8 @@ module Main (C:CONSOLE) (FS:KV_RO) (S:STACKV4) (N0:NETWORK) = struct
         let conn_closed (_,conn_id) =
           let cid = Cohttp.Connection.to_string conn_id  in
           (*C.log c (sprintf "conn %s closed" cid); (* For debugging *) *)
-          () in
+          log := ""; 
+          in
         Conduit_mirage.with_tcp conduit stackv4 stack >>= fun conduit ->
         let spec = H.make ~conn_closed ~callback:callback () in
         Conduit_mirage.listen conduit (`TCP 80) (H.listen spec));
